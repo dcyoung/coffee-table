06a478aa303b09c55bd986751130bb0d170cc3c3
dcyoung <david@ripcord.com>
2022-10-26 17:44:36 +0000

    Deploying to gh-pages from @ dcyoung/coffee-table@101b8bb4cf2cc0a23064ea3fba2057cf73304387 🚀

---
+++ assets/model.8cfa082d.js
@@ -0,0 +1,1 @@
+import{C as t,Q as o,V as a}from"./index.fdbcdf93.js";const s="/coffee-table/assets/water-draco.ccccc70f.glb",r="/coffee-table/assets/coaster-draco.e722ba15.glb",n=({...e})=>t({urls:[r,s],importRotation:0,orientation:new o().setFromAxisAngle(new a(0,1,0),0),...e});export{n as CoasterMonterey,n as default};
