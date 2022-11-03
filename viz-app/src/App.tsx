2671752c0afe340dbbfbcf0f98d0067d546f7a32
dcyoung <david@questionablyartificial.com>
2022-11-03 14:14:36 -0400

    Basic snap scroll

--- viz-app/src/App.tsx
+++ viz-app/src/App.tsx
@@ -1,10 +1,15 @@
-import "./App.css";
-import HeroViewer from "./components/heroViewer";
 // import SalesPage from "./components/sales/salesPage";
+// const App = (): JSX.Element => {
+//   return <SalesPage />;
+// };
 
+import HeroViewer from "./components/heroViewer";
 const App = (): JSX.Element => {
-  return <HeroViewer />;
-  // return <SalesPage />;
+  return (
+    <div className="fixed m-0 h-full w-screen overflow-hidden bg-white p-0 font-sans text-black">
+      <HeroViewer />
+    </div>
+  );
 };
 
 export default App;
--- viz-app/src/components/sales/salesPage.tsx
+++ viz-app/src/components/sales/salesPage.tsx
@@ -1,9 +1,18 @@
 const SalesPage = (): JSX.Element => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-slate-500 underline">
-        Hello world!
-      </h1>
+    <div className="snap h-screen max-h-screen snap-y snap-mandatory overflow-y-scroll text-center text-black">
+      {/* <div className="relative h-5/6 bg-red-400">
+        <div className="absolute top-1/4 left-1/4 h-1/2 w-1/2 bg-yellow-400"></div>
+      </div> */}
+      <div className="h-5/6 snap-center bg-red-400">
+        <h1>Page1</h1>
+      </div>
+      <div className="h-5/6 snap-center bg-blue-400">
+        <h1>Page2</h1>
+      </div>
+      <div className="h-5/6 snap-center bg-green-400">
+        <h1>Page3</h1>
+      </div>
     </div>
   );
 };
